223c0ae1138bd62452c2d3cbaa241815b2e4f597
Julius Tens <mail@juliustens.eu>
2018-03-23 14:51:58 +0100

    fix (rewrite)

--- index.js
+++ index.js
@@ -1,82 +1,10 @@
 'use strict'
 
-const db = require('db-stations')
-const mfb = require('meinfernbus')
-const lodash = require('lodash')
-const s2p = require('stream-to-promise')
-const postalCode = require('postal-code')
+const db = require('./lib/toDB')
+const mfb = require('./lib/toMFB')
 
-const plz = (zip) => {
-	if(!zip) return null
-	try{
-		const result = postalCode.get(zip)
-		if(result){
-			if(['Berlin', 'Bremen', 'Hamburg'].indexOf(result.regions[0])==-1) return result.districts[0]
-			else return result.regions[0]
-		}
-		return result
-	}
-	catch(error){return null}
+module.exports = {
+    toDB: db,
+    toMFB: mfb.toMFB,
+    toMFBRegions: mfb.toMFBRegions
 }
-
-
-const err = (error) => {throw error}
-
-const dbByStation = (station) => {
-	return s2p(db()).then((stations) => {
-		if(station.country.code=='DE'){
-			let postal = plz(station.zip)
-			if(postal){
-				let res = lodash.filter(stations, function(o){return plz(o.zip)==postal})
-				if(res && res.length>0) return res
-			}
-		}
-		return []
-	}, err)
-}
-
-const mfbByCity = (station) => {
-	return mfb.locations.stations().then((locations) => {
-		let postal = plz(station.zip)
-		if(postal){
-			let res = lodash.filter(locations, function(o){return plz(o.zip)==postal})
-			if(res && res.length>0) return mfb.locations.cities().then((cities) => lodash.filter(cities, {id: res[0].city})[0], err)
-		}
-		return []
-	}, err)
-}
-
-const search = (location) => {
-	if(location.operator==='db'){
-		return s2p(db()).then((stations) => {
-			let res = lodash.find(stations, {id: +location.id})
-			if(res){
-				return mfbByCity(res)
-			}
-			else err(new Error('DB station not found for given ID.'))
-		}, err)
-	}
-	else if(location.operator==='meinfernbus'){
-		let mfblocations
-		if(location.type==='station'){mfblocations = mfb.locations.stations}
-		else if(location.type==='city'){mfblocations = mfb.locations.cities}
-		else err(new Error('Invalid Meinfernbus location type.'))
-		return mfblocations().then((locations) => {
-			let res = lodash.find(locations, {id: location.id})
-			if(res){
-				if(location.type==='city'){
-					return search({type: 'station', operator: location.operator, id: res.stations[0]})
-				}
-				else{
-					return dbByStation(res)
-				}
-			}
-			else err(new Error('Meinfernbus location not found for given ID.'))
-		}, err)
-	}
-	else{
-		return Promise.reject(new Error('Invalid location operator.'))
-	}
-}
-
-module.exports = search
--- test.js
+++ test.js
@@ -1,15 +1,39 @@
 'use strict'
 
+const tape = require('tape')
+const validate = require('validate-fptf')
 const cities = require('./index')
 
-const works = (text) => {console.log('works')}
-const fails = (err) => {
-	console.error(err.stack || err.message)
-	process.exit(1)
-}
+tape('db-meinfernbus-cities.toDB', async (t) => {
+	const stuttgartResults = await cities.toDB({type: 'region', id: '101'}) // Stuttgart
+	t.ok(stuttgartResults.length > 5, 'results length')
+	stuttgartResults.every(r => validate(r))
+	const [stuttgart] = stuttgartResults
+	t.ok(stuttgart.id === '8000096' || (stuttgart.additionalIds && stuttgart.additionalIds.some(i => i === '8000096')), 'stuttgart id')
+	t.ok(stuttgart.name === 'Stuttgart Hbf', 'stuttgart name')
 
-cities({type: 'station', id: 1, operator: 'meinfernbus'}).then(works, fails)
-cities({type: 'city', id: 88, operator: 'meinfernbus'}).then(works, fails)
-cities({type: 'city', id: 96, operator: 'meinfernbus'}).then(works, fails)
-cities({id: 8089079, operator: 'db'}).then(works, fails)
-cities({id: 8010205, operator: 'db'}).then(works, fails)
+	const potsdamResults = await cities.toDB('2855') // Potsdam
+	t.ok(potsdamResults.length > 2, 'results length')
+	potsdamResults.every(r => validate(r))
+	const [potsdam] = potsdamResults
+	t.ok(potsdam.id === '8012666' || (potsdam.additionalIds && potsdam.additionalIds.some(i => i === '8012666')), 'potsdam id')
+	t.ok(potsdam.name === 'Potsdam Hbf', 'potsdam name')
+
+	t.end()
+})
+
+tape('db-meinfernbus-cities.toMFB', async (t) => {
+	const results = await cities.toMFB('8002841') // Münster-Hiltrup
+	t.ok(results.length > 0, 'results length')
+	results.every(r => validate(r))
+	t.ok(results.some(r => r.type === 'station' && r.id === '231' && r.name === 'Münster'), 'station münster')
+	t.end()
+})
+
+tape('db-meinfernbus-cities.toMFBRegions', async (t) => {
+	const results = await cities.toMFBRegions('8098553') // Hamburg Altona
+	t.ok(results.length === 1, 'results length')
+	results.every(r => validate(r))
+	t.ok(results.some(r => r.type === 'region' && r.id === '118' && r.name === 'Hamburg'), 'region hamburg')
+	t.end()
+})
